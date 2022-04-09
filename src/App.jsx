c61b0eed72304b6decca64b49c96fe85268884e6
Ric Taglietti <rictaglietti@gmail.com>
2022-04-09 13:54:25 -0400

    add nft to opensea

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { useAddress, useMetamask } from '@thirdweb-dev/react';
+import { useAddress, useMetamask, useEditionDrop } from '@thirdweb-dev/react';
+import { useState, useEffect } from 'react';
 
 const App = () => {
   // Use the hooks thirdweb give us.
@@ -6,24 +7,84 @@ const App = () => {
   const connectWithMetamask = useMetamask();
   console.log('👋 Address:', address);
 
+  // Initialize our editionDrop contract
+  const editionDrop = useEditionDrop(
+    '0x3F2328C90A59ABEA9F001756587Cee08Ea24C5be'
+  );
+  // State variable for us to know if user has our NFT.
+  const [hasClaimedNFT, setHasClaimedNFT] = useState(false);
+  // isClaiming lets us easily keep a loading state while the NFT is minting.
+  const [isClaiming, setIsClaiming] = useState(false);
+
+  useEffect(() => {
+    // If they don't have an connected wallet, exit!
+    if (!address) {
+      return;
+    }
+
+    const checkBalance = async () => {
+      try {
+        const balance = await editionDrop.balanceOf(address, 0);
+        if (balance.gt(0)) {
+          setHasClaimedNFT(true);
+          console.log('🌟 this user has a membership NFT!');
+        } else {
+          setHasClaimedNFT(false);
+          console.log("😭 this user doesn't have a membership NFT.");
+        }
+      } catch (error) {
+        setHasClaimedNFT(false);
+        console.error('Failed to get balance', error);
+      }
+    };
+    checkBalance();
+  }, [address, editionDrop]);
+
+  const mintNft = async () => {
+    try {
+      setIsClaiming(true);
+      await editionDrop.claim('0', 1);
+      console.log(
+        `🌊 Successfully Minted! Check it out on OpenSea: https://testnets.opensea.io/assets/${editionDrop.getAddress()}/0`
+      );
+      setHasClaimedNFT(true);
+    } catch (error) {
+      setHasClaimedNFT(false);
+      console.error('Failed to mint NFT', error);
+    } finally {
+      setIsClaiming(false);
+    }
+  };
+
   // This is the case where the user hasn't connected their wallet
   // to your web app. Let them call connectWallet.
   if (!address) {
     return (
       <div className="landing">
-        <h1>Welcome to NarutoDAO</h1>
+        <h1>Welcome to Crypt-o-Care DAO</h1>
         <button onClick={connectWithMetamask} className="btn-hero">
           Connect your wallet
         </button>
       </div>
     );
   }
+  // Add this little piece!
+  if (hasClaimedNFT) {
+    return (
+      <div className="member-page">
+        <h1>Crypt-o-Care Member Page</h1>
+        <p>Congratulations on being a member</p>
+      </div>
+    );
+  }
 
-  // This is the case where we have the user's address
-  // which means they've connected their wallet to our site!
+  // Render mint nft screen.
   return (
-    <div className="landing">
-      <h1>👀 wallet connected, now what!</h1>
+    <div className="mint-nft">
+      <h1>Mint your free Crypt-o-Care NFT</h1>
+      <button disabled={isClaiming} onClick={mintNft}>
+        {isClaiming ? 'Minting...' : 'Mint your nft (FREE)'}
+      </button>
     </div>
   );
 };
